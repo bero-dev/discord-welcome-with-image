5a72ba2be62547dc1f0a901cf5309c3552ebc617
bero <82473611+bero-dev@users.noreply.github.com>
2023-11-19 21:15:07 +0100

    image_welcomer.py

--- image_welcomer.py
+++ image_welcomer.py
@@ -33,7 +33,7 @@ async def on_ready():
 
 # Code to round the Image (Profilepicture)
 def circle(pfp,size = (215,215)):
-    pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")    
+    pfp = pfp.resize(size, Image.LANCZOS).convert("RGBA")    
     
     bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
     mask = Image.new('L', bigsize, 0)
@@ -41,7 +41,7 @@ def circle(pfp,size = (215,215)):
     draw = ImageDraw.Draw(mask) 
     draw.ellipse((0, 0) + bigsize, fill=255)
     
-    mask = mask.resize(pfp.size, Image.ANTIALIAS)
+    mask = mask.resize(pfp.size, Image.LANCZOS)
     mask = ImageChops.darker(mask, pfp.split()[-1])
     
     pfp.putalpha(mask)
